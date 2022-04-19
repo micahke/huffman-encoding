659ca88ae14adb2482d2b26f49b6203f7b683787
micahke <micahkelias@gmail.com>
2022-04-19 09:49:02 -0400

    removed images

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,17 +1,17 @@
-const git_light = require("../imgs/git-light.png");
-const m_logo = require("../imgs/m-logo.png");
+// const git_light = require("../imgs/git-light.png");
+// const m_logo = require("../imgs/m-logo.png");
 
 export default function Navbar() {
     return (
         <div className="navbar-main">
             <h2>Huffman Encoding Demo</h2>
             <div className="logo-name">
-                <a href="https://www.micahelias.com" target="_blank">
+                {/* <a href="https://www.micahelias.com" target="_blank">
                     <img className="git-light" src={m_logo} alt="logo" />
                 </a>
                 <a href="https://www.github.com/yumyumcurryman/huffman-encoding" target="_blank">
                     <img className="git-light" src={git_light} alt="logo" />
-                </a>
+                </a> */}
                 <h3>Micah Elias</h3>
             </div>
         </div>
